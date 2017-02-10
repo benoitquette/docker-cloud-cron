672f63b9dd4e5ea76cde006fede0f7e1d5f6b393
Benoit <benoit.quette@gmail.com>
2017-02-10 13:19:24 +0100

    cleanup modules

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -4,64 +4,23 @@
  * Module dependencies.
  */
 
-const mongoose = require('../models/init');
-const config = require('config');
-const amqpConfig = config.get('amqp');
-const crawlerConfig = config.get('crawler');
 const _ = require('lodash');
 const cron = require('node-cron');
-const amqp = require('amqplib/callback_api');
 
 // End of dependencies.
 
-const EXCHANGE = 'crawl';
+// look for all services with the label 'docker-cloud.schedule'
 
-// start the scheduler that will trigger the audits according to the pattern
-// define in the config file
+// for each service we start a scheduler
 cron.schedule(crawlerConfig.schedule, () => {
-  console.log('starting audits...');
-  audit();
+  startService();
 });
-console.log('scheduler started');
 
-/**
- * Loads all the projects from the db and run the audit of each of them by
- * creating a crawling task.
- * @param {function} done when done
- */
-function audit(done) {
-  const Project = mongoose.model('Project');
-  const nodeUrl = amqpConfig.connect;
-  amqp.connect(nodeUrl, (err, conn) => {
-    if (err) return console.log(err);
-    console.log('connected to node');
-    conn.createChannel((err, ch) => {
-      if (err) console.log(err);
-      console.log('connected to channel');
-      ch.assertExchange(EXCHANGE, 'fanout', {durable: true});
-      console.log('exchange is ok. now getting projects');
-      // loop through the projects and send a crawl task
-      Project.find((err, projects) => {
-        if (err) console.log(err);
-        console.log(`found ${projects.length} projects`);
-        return _.each(projects, project => {
-          const message = JSON.stringify({
-            url: project.url,
-            project: project._id, // eslint-disable-line
-            nodeUrl
-          });
-          ch.publish(EXCHANGE, '', new Buffer(message));
-          console.log(`message published: ${message}`);
-        });
-      });
-      setTimeout(() => { // close the rabbitmq connection
-        conn.close();
-        if (done) done();
-      }, 5000);
-    });
-  });
+function scheduleService(name, schedule) {
+  console.log(`Starting scheduler for service ${name}...`);
+  console.log(`Starting scheduler for service ${name}: OK`);
 }
 
-module.exports = {
-  audit
-};
+function startService() {
+
+}

879898763f742b7e07b6d208437d75ae30f03a7d
Benoit <benoit.quette@gmail.com>
2017-02-10 13:50:38 +0100

    retrieve API key from env

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -9,18 +9,32 @@ const cron = require('node-cron');
 
 // End of dependencies.
 
-// look for all services with the label 'docker-cloud.schedule'
+const LABEL = 'docker-cloud-cron.schedule';
 
-// for each service we start a scheduler
-cron.schedule(crawlerConfig.schedule, () => {
-  startService();
+// retrieve API key
+const apiKey = process.env.DOCKERCLOUD.DOCKERCLOUD_AUTH;
+console.log(`Retrieved API key: ${apiKey}`);
+
+// look for all services with the label 'docker-cloud-cron.schedule'
+const services = getServicesByLabel(LABEL);
+_.each(services, service => {
+  startService(service.name, service.schedule);
 });
 
 function scheduleService(name, schedule) {
-  console.log(`Starting scheduler for service ${name}...`);
-  console.log(`Starting scheduler for service ${name}: OK`);
+  console.log(`Scheduling service ${name}...`);
+  cron.schedule(schedule, () => {
+    startService(name);
+  });
+  console.log(`Scheduling service ${name}: OK`);
 }
 
-function startService() {
+function startService(name) {
 
+}
+
+function getServicesByLabel(label) {
+  console.log(`Looking for services with label ${label}...`);
+  console.log(`Looking for services with label ${label}: OK`);
+  return [];
 }

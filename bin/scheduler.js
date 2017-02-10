35807bf9adab03b037155b72a3638f8538effe9a
Benoit <benoit.quette@gmail.com>
2017-02-10 18:01:45 +0100

    Initial implementation

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -4,37 +4,42 @@
  * Module dependencies.
  */
 
+require('isomorphic-fetch');
 const _ = require('lodash');
 const cron = require('node-cron');
+const async = require('async');
+const dockerCloud = require('../lib/docker-cloud');
 
 // End of dependencies.
 
-const LABEL = 'docker-cloud-cron.schedule';
+const DEFAULT_LABEL = 'docker-cloud-cron.schedule';
+const DEFAULT_API_LIMIT = 100;
 
-// retrieve API key
+const label = !_.isNil(process.env.LABEL) ? process.env.LABEL : DEFAULT_LABEL;
+const apiLimit = !_.isNil(process.env.API_LIMIT) ? process.env.API_LIMIT : DEFAULT_API_LIMIT;
 const apiKey = process.env.DOCKERCLOUD_AUTH;
-console.log(`Retrieved API key: ${apiKey}`);
+const stack = process.env.DOCKERCLOUD_STACK_NAME;
 
-// look for all services with the label 'docker-cloud-cron.schedule'
-const services = getServicesByLabel(LABEL);
-_.each(services, service => {
-  startService(service.name, service.schedule);
-});
-
-function scheduleService(name, schedule) {
-  console.log(`Scheduling service ${name}...`);
-  cron.schedule(schedule, () => {
-    startService(name);
+if (_.isNil(apiKey) || _.isEmpty(apiKey)) {
+  console.log(`Missing API key. Please make sure the service is running
+  with the 'global' role so it can have access to the node environment.`);
+} else {
+  // look for all services with the appropriate label
+  dockerCloud.findServicesByLabel(label, apiKey, apiLimit, (err, services) => {
+    if (err) {
+      return console.log(err);
+    }
+    // we schedule each of the service to start
+    // as specified in the label text
+    console.log(`Found ${services.length} service(s) to schedule.`);
+    return _.each(services, (service => {
+      cron.schedule(service.schedule, () => {
+        dockerCloud.startService(service.uuid, apiKey, err => {
+          if (err) return console.log(err);
+          console.log(`Request to start service ${uuid} sent successfuly.`);
+        })
+      });
+      console.log(`Scheduled service ${service.name} to ${service.schedule}.`);
+    }));
   });
-  console.log(`Scheduling service ${name}: OK`);
-}
-
-function startService(name) {
-
-}
-
-function getServicesByLabel(label) {
-  console.log(`Looking for services with label ${label}...`);
-  console.log(`Looking for services with label ${label}: OK`);
-  return [];
 }
